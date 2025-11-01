2920058b2e65371c00be89398a91fc71a082c6c5
tranlongdollar <tranlong.dollar@gmail.com>
2025-11-01 19:56:13 +0700

    Update config.php

--- massagenow.vn/app/config.php
+++ massagenow.vn/app/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'massagenow_news');
-define('DB_USER', 'massagenow_news');
-define('DB_PASS', 'Kit@1234567');
+define('DB_NAME', 'mysql');
+define('DB_USER', 'mysql');
+define('DB_PASS', 'passwordsql');
 define('DB_CHARSET', 'utf8mb4');
 define('MAIL_FROM',      'no-reply@massagenow.vn');   // đổi thành domain của anh
 define('MAIL_FROM_NAME', 'MassageNow');
@@ -12,3 +12,4 @@ define('MAIL_ADMIN_TO',  'booking@massagenow.vn');
 define('DEFAULT_LANG', 'vi');
 // Base URL (điều chỉnh đúng domain thực tế, không dấu / ở cuối)
 define('BASE_URL', 'https://massagenow.vn');
+
